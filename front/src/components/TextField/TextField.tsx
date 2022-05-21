1dca8852e4c895ee210ea73ffae90b5a7e1adff3
Albert Dinh Personal <adinh.1867@gmail.com>
2022-05-20 20:39:38 -0600

    Init good

---
+++ front/src/components/TextField/TextField.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TextField = () => {
+  return (
+    <div id="textField" className="">TextField</div>
+  )
+}
+
+export default TextField
---
+++ front/src/components/WPMGraph/WPMGraph.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WPMGraph = () => {
+  return (
+    <div id="wpmGraph">WPMGraph</div>
+  )
+}
+
+export default WPMGraph

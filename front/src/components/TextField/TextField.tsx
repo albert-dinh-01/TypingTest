518de9bdb8c45ca7770ab8712686e49ed8be2833
Albert Dinh Personal <adinh.1867@gmail.com>
2022-05-22 14:36:11 -0600

    Random words...

--- front/src/components/TextField/TextField.tsx
+++ front/src/components/TextField/TextField.tsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import "./styles/TextField.css";
 
 const getText = () =>
-	`Sleep deprivation causes all sorts of challenges and `
+	`Sleep deprivation causes all sorts of challenges and problems When one get enough sleep mind work clearly Studies have shown that after staying awake for 24 hours ability to do simple math is greatly impaired Driving tired has been shown to be as bad as driving drunk Moods change depression anxiety and mania can be induced by lack of sleep As much as people try to do without enough sleep it is a wonder more crazy things happen in this world There was something in the tree It was difficult to tell from the ground but Rachael could see movement She squinted her eyes and peered in the direction of the movement trying to decipher exactly what she had spied The more she peered however the more she thought it might be a figment of her imagination Nothing seemed to move until the moment she began to take her eyes off the tree Then in the corner of her eye she would see the movement again and begin the process of staring again`
 		.split(" ")
 		.sort(() => (Math.random() > 0.5 ? 1 : -1));
 

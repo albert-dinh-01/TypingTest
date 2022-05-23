53a403cfb443637f11a6183af94382737e7366a1
Albert Dinh Personal <adinh.1867@gmail.com>
2022-05-23 15:47:59 -0600

    Got rid of the Footer component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./App.css";
 import TextField from "./components/TextField/TextField";
 import WelcomeText from "./components/WelcomeText/WelcomeText";
-import Footer from "./components/Footer/Footer";
 
 const App = () => {
 	return (
-		<div className="relative top-0 w-[100vw] h-[100vh] sm:grid sm:grid-cols-1 sm:bg-black" id="app">
+		<div
+			className="relative top-0 w-[100vw] h-[100vh] sm:grid sm:grid-cols-1 sm:bg-black"
+			id="app"
+		>
 			<WelcomeText />
 			<TextField />
-			<Footer />
 		</div>
 	);
 };

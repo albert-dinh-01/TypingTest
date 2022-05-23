81790dccc30ece50e496c5fd91546c26e210c539
Albert Dinh Personal <adinh.1867@gmail.com>
2022-05-23 12:39:12 -0600

    Added the footer for the app

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 import TextField from "./components/TextField/TextField";
 import WelcomeText from "./components/WelcomeText/WelcomeText";
+import Footer from "./components/Footer/Footer";
+
 const App = () => {
 	return (
 		<div className="" id="app">
 			<WelcomeText />
 			<TextField />
+			<Footer />
 		</div>
 	);
 };

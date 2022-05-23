8ec116e3258651ea7c212eb91858ca2357a3a28a
Albert Dinh Personal <adinh.1867@gmail.com>
2022-05-23 12:37:59 -0600

    Added the welcome text component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
 import TextField from "./components/TextField/TextField";
-
+import WelcomeText from "./components/WelcomeText/WelcomeText";
 const App = () => {
 	return (
 		<div className="" id="app">
+			<WelcomeText />
 			<TextField />
 		</div>
 	);

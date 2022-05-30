be1335b5db153aa16c1f4ea9e1b064918eda5c45
Albert Dinh Personal <adinh.1867@gmail.com>
2022-05-29 19:40:35 -0600

    Good natural position

--- front/src/components/TextField/TextField.tsx
+++ front/src/components/TextField/TextField.tsx
@@ -124,7 +124,7 @@ const TextFieldComponent = () => {
 	};
 
 	return (
-		<div id="textFieldContainer" className="relative bg-red-600 top-0 h-auto max-h-[50%] w-[55vw] max-w-3xl">
+		<div id="textFieldContainer" className="relative bg-red-600 top-[0px] h-auto max-h-[50%] w-[55vw] max-w-3xl">
 			<div className="" id="timer">
 				<Timer
 					startCount={startCountYet}
--- front/src/components/WelcomeText/WelcomeText.tsx
+++ front/src/components/WelcomeText/WelcomeText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WelcomeText = () => {
 	return (
-		<div className="relative bg-sky-800 top-[200px]" id="welcomeContainer">
+		<div className="relative bg-purple-800 top-[0px]" id="welcomeContainer">
 			<p className="" id="welcomeText">
 				Welcome to Typing Speed Test
 			</p>

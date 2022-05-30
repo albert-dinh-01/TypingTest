7571e471d8220b7c7de7e2f4dc3d42d393ad7615
Albert Dinh Personal <adinh.1867@gmail.com>
2022-05-29 19:47:11 -0600

    Added the header for the app

--- front/src/components/WelcomeText/WelcomeText.tsx
+++ front/src/components/WelcomeText/WelcomeText.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const WelcomeText = () => {
 	return (
-		<div className="relative bg-purple-800 top-[0px]" id="welcomeContainer">
+		<div
+			className="relative bg-purple-800 top-[20%] left-[30%] w-auto max-w-[40%]"
+			id="welcomeContainer"
+		>
 			<p className="" id="welcomeText">
 				Welcome to Typing Speed Test
 			</p>

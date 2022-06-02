243114ca923cdf29a6b030e7e03fff72fea6fd6f
Albert Dinh Personal <adinh.1867@gmail.com>
2022-06-01 20:11:28 -0600

    Header top repositioned

--- front/src/components/WelcomeText/WelcomeText.tsx
+++ front/src/components/WelcomeText/WelcomeText.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const WelcomeText = () => {
 	return (
 		<div
-			className="absolute bg-purple-800 top-[10%] left-[30%] h-auto max-h-[15%] w-auto max-w-[40%]"
+			className="absolute bg-purple-800 left-[30%] h-auto max-h-[15%] w-auto max-w-[40%] top-[5%]"
 			id="welcomeContainer"
 		>
 			<p className="" id="welcomeText">

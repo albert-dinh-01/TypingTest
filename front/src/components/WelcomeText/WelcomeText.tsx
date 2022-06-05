8a8869b9e8e6bf78f985c6ec0336eb5c6d8ccc6a
Albert Dinh Personal <adinh.1867@gmail.com>
2022-06-05 12:59:10 -0600

    Header position and font size modified for smaller screen sizes

--- front/src/components/WelcomeText/WelcomeText.tsx
+++ front/src/components/WelcomeText/WelcomeText.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const WelcomeText = () => {
 	return (
 		<div
-			className="absolute bg-fuchsia-500 left-[15%] h-auto w-auto top-[5%] text-[3rem]"
+			className="absolute bg-fuchsia-500 left-[15%] h-auto w-auto top-[5%] text-[3rem] sm:top-0 sm:left-[10%] sm:text-[2rem]"
 			id="welcomeContainer"
 		>
 			<p className="" id="welcomeText">

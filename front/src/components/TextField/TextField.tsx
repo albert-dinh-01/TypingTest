772e7ae6655faffb1a743079c032d9b0866c6c2f
Albert Dinh Personal <adinh.1867@gmail.com>
2022-06-09 21:01:31 -0600

    Added todos for completing in the future

--- front/src/components/TextField/TextField.tsx
+++ front/src/components/TextField/TextField.tsx
@@ -6,9 +6,11 @@ import TextField from "@mui/material/TextField";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 
-// TODO: Give another 10 words once all 10 words are covered
 // TODO: Identify why I can type more than one space SOMETIMES
 // TODO: Fix the problem where the last word of the next 10 words will be coloured red
+// TODO: Fix the problem where typing separate words doesn't transfer to the next word
+// TODO: 
+
 const getText = () =>
 	`Sleep deprivation causes all sorts of challenges and problems When one get enough sleep mind work clearly Studies have shown that after staying awake for 24 hours ability to do simple math is greatly impaired Driving tired has been shown to be as bad as driving drunk Moods change depression anxiety and mania can be induced by lack of sleep As much as people try to do without enough sleep it is a wonder more crazy things happen in this world There was something in the tree It was difficult to tell from the ground but Rachael could see movement She squinted her eyes and peered in the direction of the movement trying to decipher exactly what she had spied The more she peered however the more she thought it might be a figment of her imagination Nothing seemed to move until the moment she began to take her eyes off the tree Then in the corner of her eye she would see the movement again and begin the process of staring again`
 		.split(" ")
@@ -149,18 +151,14 @@ const TextFieldComponent = () => {
 		if (e.endsWith(" ")) {
 			console.log("after someone presses spacebar");
 			if (activeWordIndex === text.current.length - 1) {
-				// setStartCountYet(false);
+				console.log("9th word");
 				setUserInput("");
 				text.current = getText();
 				setCorrectWordArray([]);
 			} else {
 				setUserInput("");
 			}
-
-			if (activeWordIndex === text.current.length - 1) {
-				setActiveWordIndex(0);
-			}
-
+			
 			if (activeWordIndex === text.current.length - 1) {
 				setActiveWordIndex(0);
 			} else {

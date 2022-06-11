b83b499c546fc1b84381ef548d775fbc3ba9ce5c
Albert Dinh Personal <adinh.1867@gmail.com>
2022-06-11 12:45:16 -0600

    Added some resizing into the displayed word window

--- front/src/components/TextField/TextField.tsx
+++ front/src/components/TextField/TextField.tsx
@@ -223,12 +223,12 @@ const TextFieldComponent = () => {
 			</div>
 
 			<div
-				className="w-[45vw] h-[400px] bg-black sm:w-full"
+				className="w-[45vw] h-[400px] sm:h-auto bg-black sm:w-full"
 				id="typingContainer"
 			>
 				<p
 					id="displayedTest"
-					className="bg-lime-800 h-[50%] text-[2rem] sm:text-[1.2rem] text-center pt-14"
+					className="bg-lime-800 h-[50%] text-[2rem] sm:text-[1.2rem] text-center pt-14 sm:pt-2 sm:h-auto"
 				>
 					{text.current.map((word, index) => {
 						return (
